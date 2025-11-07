4a47657d4d7080dd812b2953d072ee33839efcae
Admin <landrzejewski.poczta@gmail.com>
2025-11-07 15:13:21 +0100

    security intro

--- src/main/java/pl/training/ApplicationConfiguration.java
+++ src/main/java/pl/training/ApplicationConfiguration.java
@@ -1,15 +1,8 @@
 package pl.training;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApplicationConfiguration implements WebMvcConfigurer {
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("login.html").setViewName("login-form");
-    }
-
 }
--- src/main/java/pl/training/SecurityConfiguration.java
+++ src/main/java/pl/training/SecurityConfiguration.java
@@ -3,10 +3,15 @@ package pl.training;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import pl.training.security.KeycloakAuthoritiesConverter;
+import pl.training.security.KeycloakAuthoritiesMapper;
+import pl.training.security.KeycloakLogoutHandler;
 
 import java.util.List;
 
@@ -33,12 +38,24 @@ public class SecurityConfiguration {
                 .csrf(config -> config.ignoringRequestMatchers("/api/**"))
                 .cors(config -> config.configurationSource(request -> corsConfiguration()))
                 .oauth2ResourceServer(config -> config.jwt(withDefaults()))
+                .oauth2Login(config -> config.userInfoEndpoint(this::userInfoCustomizer))
                 .authorizeHttpRequests(config -> config
                                 .requestMatchers(GET, "/actuator").permitAll()
                                 .anyRequest().hasRole("ADMIN") //.authenticated()
                                 // .anyRequest().access(new TimeBasedAuthorizationManager())
                 )
+                .logout(config -> config
+                        .logoutRequestMatcher(requestMatcherBuilder().matcher("/logout.html"))
+                        .logoutSuccessUrl("/login.html")
+                        .invalidateHttpSession(true)
+                        .addLogoutHandler(new KeycloakLogoutHandler(new RestTemplate()))
+                )
                 .build();
+    }
+
+    @Bean
+    PathPatternRequestMatcher.Builder requestMatcherBuilder() {
+        return PathPatternRequestMatcher.withDefaults();
     }
 
     @Bean
@@ -46,6 +63,12 @@ public class SecurityConfiguration {
         var jwtConverter = new JwtAuthenticationConverter();
         jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakAuthoritiesConverter());
         return jwtConverter;
+    }
+
+
+    // Client scopes -> Client scope details (roles) -> Mapper details -> Add to userinfo enabled (Keycloak Admin console)
+    private void userInfoCustomizer(OAuth2LoginConfigurer<HttpSecurity>.UserInfoEndpointConfig userInfoEndpointConfig) {
+        userInfoEndpointConfig.userAuthoritiesMapper(new KeycloakAuthoritiesMapper());
     }
 
 }
