8b8d54db143fafa7d903644a4286d5b7ebde5831
Admin <landrzejewski.poczta@gmail.com>
2026-01-08 11:01:31 +0100

    update

---
+++ src/main/java/pl/training/payments/adapters/persistence/mongo/MongoCardRepository.java
@@ -0,0 +1,13 @@
+package pl.training.payments.adapters.persistence.mongo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Optional;
+
+public interface MongoCardRepository extends MongoRepository<CardDocument, String> {
+
+    // @Query("")
+    Optional<CardDocument> findByNumber(String number);
+
+}
