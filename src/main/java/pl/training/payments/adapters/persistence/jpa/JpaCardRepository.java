aa0dc399fa05786771fce4559713dc20588bddd8
Admin <landrzejewski.poczta@gmail.com>
2026-01-09 16:15:51 +0100

    update

--- src/main/java/pl/training/payments/adapters/persistence/jpa/JpaCardRepository.java
+++ src/main/java/pl/training/payments/adapters/persistence/jpa/JpaCardRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-interface JpaCardRepository extends JpaRepository<CardEntity, String> { // CrudRepository<CardEntity, String> // Repository<CardEntity, String>
+public interface JpaCardRepository extends JpaRepository<CardEntity, String> { // CrudRepository<CardEntity, String> // Repository<CardEntity, String>
 
     // @Query("select c from Card c where c.number = :cardNumber")
     Optional<CardEntity> findByNumber(String cardNumber);
